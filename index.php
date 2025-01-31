5864b600c41d7df7f22aa6a574db07dbbd012f26
ManniAufDen <fynlinkrieg@gmail.com>
2025-01-31 11:39:42 +0100

    Junge Lars!!

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <body>
     <h1>Hello, World!</h1>
-    <h2> Ihr Wichser!!! </h2>
+    <h2>Ihr Wichser!!!</h2>
     <h3>Test123</h3>
+    <h4>NochMal ein Test</h4>
 </body>
 </html>
